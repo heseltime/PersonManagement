4c938082d2b6224c363662e7aefb0f9650c78e98
Jack Heseltine <66922223+heseltime@users.noreply.github.com>
2023-09-30 14:07:39 +0200

    Add project files.

---
+++ PersonManagement/PersonRepository.cs
@@ -0,0 +1,42 @@
+﻿namespace PersonManagement;
+
+public class PersonRepository
+{
+    private readonly IList<Person> persons = new List<Person>();
+
+    public void AddPerson(Person person)
+    {
+
+    }
+
+    public void AddPersons(IEnumerable<Person> persons)
+    {
+
+    }
+
+    public void PrintPersons(TextWriter textWriter)
+    {
+
+    }
+
+    public IEnumerable<(string?, string?)> GetPersonNames()
+    {
+        return null;
+    }
+
+    public IEnumerable<Person> FindPersonsByCity(string city)
+    {
+        return null;
+    }
+
+    public Person FindYoungestPerson()
+    {
+        return null;
+    }
+
+
+    public IEnumerable<Person> FindPersonsSortedByAgeAscending()
+    {
+        return null;
+    }
+}
---
+++ PersonManagement/Program.cs
@@ -0,0 +1,59 @@
+﻿using PersonManagement;
+
+PersonRepository personRepository = new PersonRepository();
+IEnumerable<Person>? persons = new List<Person>();
+
+try
+{
+	//
+	// TODO
+	//
+
+	var reader = new StreamReader("persons.json");
+}
+catch (FileNotFoundException fnfEx)
+{
+	Console.WriteLine(fnfEx.Message);
+	return;
+}
+
+// TODO
+
+//textWriter.WriteLine("=====================================================");
+//textWriter.WriteLine("Person list");
+//textWriter.WriteLine("=====================================================");
+//
+// TODO
+//
+
+//textWriter.WriteLine();
+//textWriter.WriteLine("=====================================================");
+//textWriter.WriteLine("Persons in Hagenberg");
+//textWriter.WriteLine("=====================================================");
+//
+// TODO
+//
+
+//textWriter.WriteLine();
+//textWriter.WriteLine("=====================================================");
+//textWriter.WriteLine("Person names");
+//textWriter.WriteLine("=====================================================");
+//
+// TODO
+//
+
+//textWriter.WriteLine();
+//textWriter.WriteLine("=====================================================");
+//textWriter.WriteLine($"Youngest person");
+//textWriter.WriteLine("=====================================================");
+//
+// TODO
+//
+
+//textWriter.WriteLine();
+//textWriter.WriteLine("=====================================================");
+//textWriter.WriteLine("Persons sorted by age ascending");
+//textWriter.WriteLine("=====================================================");
+//
+// TODO
+//
